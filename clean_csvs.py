7cddb848c68d8f0a2641cc9d6b87866487e6580d
krasian <XYZ@example.com>
2026-04-11 22:49:51 +0200

    fixing path error

--- clean_csvs.py
+++ clean_csvs.py
@@ -7,5 +7,6 @@ files = os.listdir(INPUT_FOLDER)
 
 for file in files:
     if file.endswith(".csv"):
+        path = os.path.join(INPUT_FOLDER, file)
         df = pd.read_csv(file)
         print(df.head())
