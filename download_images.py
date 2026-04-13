fcb8c9ba0449ec873ab749b7df17d9bddfad8d24
krasian <XYZ@example.com>
2026-04-13 23:10:30 +0200

    skeleton for image downloading

---
+++ download_images.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import os
+import requests
+from tqdm import tqdm
+
+INPUT_FOLDER = "data/clean_csv"
+OUTPUT_FOLDER = "data/images"
+
+TIMEOUT = 10
+
+
+def download_image(url, save_path):
+    pass
+
+
+def process_csv(file_path):
+    pass
+
+
+def main():
+    os.makedirs(OUTPUT_FOLDER, exist_ok=True)
+
+    files = [f for f in os.listdir(INPUT_FOLDER) if f.endswith("_clean.csv")]
+
+    for file in files:
+        path = os.path.join(INPUT_FOLDER, file)
+        process_csv(path)
+
+
+if __name__ == "__main__":
+    main()
