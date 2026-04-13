817a1058da57b4ffcb6e043333c389dc8ae4c138
krasian <XYZ@example.com>
2026-04-13 22:09:23 +0200

    dd research grade filter and files

--- clean_csvs.py
+++ clean_csvs.py
@@ -8,10 +8,13 @@ OUTPUT_FOLDER = "data/clean_csv"
 TRAIN_SPECIES = ["blackbird", "blue_tit", "great_tit", "sparrow"]
 TEST_SPECIES = ["robin", "crow", "starling", "thrush","flamingo","toucan"]
 
-MAX_SAMPLES = 2000
+MAX_SAMPLES = 2300
 
 # clean function
 def clean_df(df, country=None):
+
+    if "quality_grade" in df.columns:
+        df = df[df["quality_grade"] == "research"]
 
     df = df.dropna(subset=["image_url"])
     df = df.drop_duplicates(subset=["image_url"])
@@ -23,39 +26,38 @@ def clean_df(df, country=None):
 
 os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 
-files = os.listdir(INPUT_FOLDER)
+files = [f for f in os.listdir(INPUT_FOLDER) if f.endswith(".csv")]
 
 for file in files:
-    if not file.endswith(".csv"):
-        continue
+
+    path = os.path.join(INPUT_FOLDER, file)
+    name = file.lower()
 
     print(f"\nProcessing: {file}")
 
-    path = os.path.join(INPUT_FOLDER, file)
-    df = pd.read_csv(path)   # fixed
-
-    name = file.lower()
+    df = pd.read_csv(path)
 
     if any(sp in name for sp in TRAIN_SPECIES):
-        print("train")
+        print("TRAIN species (Netherlands filter)")
         df = clean_df(df, country="Netherlands")
 
     elif any(sp in name for sp in TEST_SPECIES):
-        print("test")
+        print("TEST species (no country filter)")
         df = clean_df(df)
 
     else:
+        print("SKIPPED (unknown species)")
         continue
 
-    if len(df) > MAX_SAMPLES:
+    if MAX_SAMPLES and len(df) > MAX_SAMPLES:
         df = df.sample(MAX_SAMPLES, random_state=42)
+
+    print("Final rows:", len(df))
 
     out_name = file.replace(".csv", "_clean.csv")
     out_path = os.path.join(OUTPUT_FOLDER, out_name)
 
     df.to_csv(out_path, index=False)
-
-    print("Final rows:", len(df))
     print("Saved:", out_path)
 
 
