1692254e4a8024f64a425569bbd6d680978a21c0
krasian <XYZ@example.com>
2026-04-13 21:59:42 +0200

    arange more logically and fixed correct file path

--- clean_csvs.py
+++ clean_csvs.py
@@ -2,27 +2,13 @@ import pandas as pd
 import os
 
 INPUT_FOLDER = "data/raw_csv"
-
-files = os.listdir(INPUT_FOLDER)
-
-for file in files:
-    if file.endswith(".csv"):
-        path = os.path.join(INPUT_FOLDER, file)
-        df = pd.read_csv(file)
-        print(df.head())
-
+OUTPUT_FOLDER = "data/clean_csv"
 
 # seperate train and test species
 TRAIN_SPECIES = ["blackbird", "blue_tit", "great_tit", "sparrow"]
 TEST_SPECIES = ["robin", "crow", "starling", "thrush","flamingo","toucan"]
 
-name = file.lower()
-
-if any(sp in name for sp in TRAIN_SPECIES):
-    print("train")
-
-elif any(sp in name for sp in TEST_SPECIES):
-    print("test")
+MAX_SAMPLES = 2000
 
 # clean function
 def clean_df(df, country=None):
@@ -35,24 +21,42 @@ def clean_df(df, country=None):
 
     return df
 
-
-OUTPUT_FOLDER = "data/clean_csv"
 os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 
-out_name = file.replace(".csv", "_clean.csv")
-out_path = os.path.join(OUTPUT_FOLDER, out_name)
-df.to_csv(out_path, index = False)
+files = os.listdir(INPUT_FOLDER)
 
-MAX_SAMPLES = 2000
+for file in files:
+    if not file.endswith(".csv"):
+        continue
 
-if len(df) > MAX_SAMPLES:
-    df = df.sample(MAX_SAMPLES, random_state =42)
+    print(f"\nProcessing: {file}")
 
+    path = os.path.join(INPUT_FOLDER, file)
+    df = pd.read_csv(path)   # fixed
 
-print(f"\nProcessing: {file}")
-print("Final rows:", len(df))
-print("Saved:", out_path)
+    name = file.lower()
 
+    if any(sp in name for sp in TRAIN_SPECIES):
+        print("train")
+        df = clean_df(df, country="Netherlands")
+
+    elif any(sp in name for sp in TEST_SPECIES):
+        print("test")
+        df = clean_df(df)
+
+    else:
+        continue
+
+    if len(df) > MAX_SAMPLES:
+        df = df.sample(MAX_SAMPLES, random_state=42)
+
+    out_name = file.replace(".csv", "_clean.csv")
+    out_path = os.path.join(OUTPUT_FOLDER, out_name)
+
+    df.to_csv(out_path, index=False)
+
+    print("Final rows:", len(df))
+    print("Saved:", out_path)
 
 
 
